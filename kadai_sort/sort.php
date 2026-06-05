89d97a1cb9e3cad0a5117dbdee17f6609008bb6e
Python869 <123977481+Python869@users.noreply.github.com>
2026-06-05 11:51:04 +0900

    フォルダをアップロードしました

---
+++ kadai_sort/sort.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        //数値を変数に格納
+        $nums = [15, 4, 18, 23, 10];
+
+        //配列と並べ順の関数を定義
+        function sort_2way($array, $order){
+            //並べ順が昇順か降順かで条件分岐
+            if ($order === true) {
+                echo  "昇順にソートします<br>";
+                sort($array);
+                //配列のデータを一行ずつ取り出す
+                foreach ($array as $num) {
+                    echo $num . "<br>";
+                }
+            } else {
+                echo "降順にソートします<br>";
+                arsort($array);
+                foreach ($array as $num) {
+                    echo $num . "<br>";
+                }
+            }
+        }
+        //関数を呼び出す
+        sort_2way($nums, true);
+        sort_2way($nums, false);
+        ?>
+    </p>
+</body>
